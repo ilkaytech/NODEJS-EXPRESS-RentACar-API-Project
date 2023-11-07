fa002c09233473082080e6f043e9eeec74e3b96c
ilkay <06ilkayozturk@gmail.com>
2023-11-07 22:48:00 +0300

    MongoDBConnection

--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -2,3 +2,23 @@
 /* -------------------------------------
     NODEJS EXPRESS | Rent A Car API
 ----------------------------------------*/
+// MongoDB Connection:
+
+const mongoose = require("mongoose");
+
+const dbConnection = function () {
+  // Connect:
+  mongoose
+    .connect(process.env.MONGODB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("* DB Connected * "))
+    .catch((err) => console.log("* DB Not Connected * ", err));
+};
+
+/* ------------------------------------------------------- */
+module.exports = {
+  mongoose,
+  dbConnection,
+};
