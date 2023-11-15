4b2ea04e80c23b3f90df74973292051058071e05
ilkay <06ilkayozturk@gmail.com>
2023-11-15 22:25:32 +0300

    feat: modelUser

--- src/models/user.js
+++ src/models/user.js
@@ -2,3 +2,78 @@
 /* -------------------------------------
     NODEJS EXPRESS | Rent A Car API
 ----------------------------------------*/
+const { mongoose } = require("../configs/dbConnection");
+/* ------------------------------------------------------- *
+{
+    "username": "admin",
+    "password": "1234",
+    "email": "admin@site.com",
+    "firstName": "admin",
+    "lastName": "admin",
+    "isActive": true,
+    "isAdmin": true
+}
+{
+    "username": "test",
+    "password": "1234",
+    "email": "test@site.com",
+    "firstName": "test",
+    "lastName": "test",
+    "isActive": true,
+    "isAdmin": false
+}
+/* ------------------------------------------------------- */
+// User Model:
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+/* ------------------------------------------------------- */
+// Schema Configs:
+
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+userSchema.pre("save", function (next) {
+  const isPasswordValidated = Regexp;
+});
