234cc2fa6393df1a7acf15d10c10ddb2ddac6323
ilkay <06ilkayozturk@gmail.com>
2023-11-21 16:47:48 +0300

    feat:reservation models created

---
+++ src/models/reservation.js
@@ -0,0 +1,50 @@
+"use strict";
+/* -------------------------------------
+    NODEJS EXPRESS | Rent A Car API
+----------------------------------------*/
+const { mongoose } = require("../configs/dbConnection");
+/* ------------------------------------------------------- *
+{
+    "userId": "65343222b67e9681f937f001",
+    "carId": "65352f518a9ea121b1ca5001",
+    "startDate": "2023-10-10",
+    "endDate": "2023-10-16"
+}
+{
+    "userId": "65343222b67e9681f937f002",
+    "carId": "65352f518a9ea121b1ca5002",
+    "startDate": "2023-10-14",
+    "endDate": "2023-10-20"
+}
+/* ------------------------------------------------------- */
+// Reservation Model:
+
+const ReservationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    carId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Car",
+      required: true,
+    },
+
+    startDate: {
+      type: Date,
+      required: true,
+    },
+
+    endDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  { collection: "reservations", timestamps: true }
+);
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("Reservation", ReservationSchema);
