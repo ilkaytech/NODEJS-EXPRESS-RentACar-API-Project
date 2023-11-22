8da1926811502f248e5c69b49d49bcd25c5f1fe0
ilkay <06ilkayozturk@gmail.com>
2023-11-22 16:08:51 +0300

    feat:routes document is created

--- src/helpers/sync.js
+++ src/helpers/sync.js
@@ -8,10 +8,10 @@ module.exports = async function () {
   // return null;
 
   /* REMOVE DATABASE *
-    const { mongoose } = require('../configs/dbConnection')
-    await mongoose.connection.dropDatabase()
-    console.log('- Database and all data DELETED!')
-    /* REMOVE DATABASE */
+  const { mongoose } = require('../configs/dbConnection')
+  await mongoose.connection.dropDatabase()
+  console.log('- Database and all data DELETED!')
+  /* REMOVE DATABASE */
 
   /* User */
   const User = require("../models/user");
@@ -19,7 +19,7 @@ module.exports = async function () {
   await User.create({
     _id: "65343222b67e9681f937f001",
     username: "admin",
-    password: "1234",
+    password: "aA*123456",
     email: "admin@site.com",
     firstName: "admin",
     lastName: "admin",
@@ -29,7 +29,7 @@ module.exports = async function () {
   await User.create({
     _id: "65343222b67e9681f937f002",
     username: "test",
-    password: "1234",
+    password: "aA*123456",
     email: "test@site.com",
     firstName: "test",
     lastName: "test",
