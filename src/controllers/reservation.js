7f3732b10e306eacfb6158a126d2f6a2f711e5c3
ilkay <06ilkayozturk@gmail.com>
2023-11-24 14:05:15 +0300

    fix:reservation

--- src/controllers/reservation.js
+++ src/controllers/reservation.js
@@ -21,11 +21,19 @@ module.exports = {
             `
         */
 
-    const data = await res.getModelList(Reservation, {}, ["userId", "carId"]);
+    // Filters:
+    let filters = {};
+    // Only self records. except admin:
+    if (!req?.user.isAdmin) filters.userId = req.user._id;
+
+    const data = await res.getModelList(Reservation, filters, [
+      "userId",
+      "carId",
+    ]);
 
     res.status(200).send({
       error: false,
-      details: await res.getModelListDetails(Reservation),
+      details: await res.getModelListDetails(Reservation, filters),
       data,
     });
   },
@@ -44,12 +52,32 @@ module.exports = {
             }
         */
 
-    const data = await Reservation.create(req.body);
+    // get userId from loginedUser.
+    req.body.userId = req?.user._id;
 
-    res.status(201).send({
-      error: false,
-      data,
+    // Check new reservations date in exists reservations:
+    const userReservationInDates = await Reservation.findOne({
+      userId: req.body.userId,
+      $nor: [
+        { startDate: { $gt: req.body.endDate } },
+        { endDate: { $lt: req.body.startDate } },
+      ],
     });
+
+    if (userReservationInDates) {
+      res.errorStatusCode = 400;
+      throw new Error(
+        "It cannot be added because there is another reservation with the same date.",
+        { cause: { userReservationInDates: userReservationInDates } }
+      );
+    } else {
+      const data = await Reservation.create(req.body);
+
+      res.status(201).send({
+        error: false,
+        data,
+      });
+    }
   },
 
   read: async (req, res) => {
@@ -58,10 +86,15 @@ module.exports = {
             #swagger.summary = "Get Single Reservation"
         */
 
-    const data = await Reservation.findOne({ _id: req.params.id }).populate([
-      "userId",
-      "carId",
-    ]);
+    // Filters:
+    let filters = {};
+    // Only self records. except admin:
+    if (!req?.user.isAdmin) filters.userId = req.user._id;
+
+    const data = await Reservation.findOne({
+      _id: req.params.id,
+      ...filters,
+    }).populate(["userId", "carId"]);
 
     res.status(200).send({
       error: false,
